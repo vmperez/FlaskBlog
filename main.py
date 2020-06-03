d55f4c0de33179bbdf0fb7d4b610ace938c08677
Victor M. Perez <awlo@hotmail.com>
2020-06-03 12:36:04 +0200

    Refactored check_db() in main.py, removed .git/ from .gitignore

--- main.py
+++ main.py
@@ -9,12 +9,8 @@ from entries.blueprint import entries
 app.register_blueprint(entries, url_prefix='/entries')
 
 def check_db():
-	if not os.path.exists('./blog.db'):
-		os.mknod('./blog.db')
-		
-		
-	# SQLite database file header is 100 bytes
-	if os.path.getsize('./blog.db') < 100:
+	if ((not os.path.exists('./blog.db') || (os.path.getsize('./blog.db') < 100)):
+		# SQLite database file header is 100 bytes
 		db.create_all()
 	else:
 		print("No need to create new DB")
